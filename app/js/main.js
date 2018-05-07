d978396b58aba605a56b9ab912d556e220075ed1
ilya199006 <soulrebel0@gmail.com>
2018-05-07 23:42:36 +0400

    v0.0.3 Реорганизация

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('.traces__slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: 'true',
+	});
+	$('.about__slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	});
+	$('.reviews__slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	});
+});
